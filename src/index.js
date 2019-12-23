3bc514033d47756c643693077026e3302b42fad1
Simon Reinisch <simon@reinisch.io>
2019-12-23 18:30:21 +0100

    Add Footer

--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ import {APIOptions}   from './app/api/api-options';
 import {APISelection} from './app/api/api-selection';
 import {IntlResult}   from './app/result/intl-result';
 import {IntlInput}    from './app/result/intl-input';
+import {Footer}       from './app/footer';
 import './register-service-worker';
 import './index.scss';
 
 render(
     <div id="app">
-        <div class="left">
-            <APISelection/>
-            <APIOptions/>
+        <div class="content">
+            <div className="left">
+                <APISelection/>
+                <APIOptions/>
+            </div>
+            <p/>
+            <div className="right">
+                <IntlInput/>
+                <IntlResult/>
+            </div>
         </div>
-        <p/>
-        <div class="right">
-            <IntlInput/>
-            <IntlResult/>
-        </div>
+        <Footer/>
     </div>,
     document.body
 );
